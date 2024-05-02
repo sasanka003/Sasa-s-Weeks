95ee9bf9cccccc2ce9ffe29ec490d42235d4adff
sasanka003 <sasankaudana@gmail.com>
2024-05-02 10:00:23 +0530

    added routing for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 function App() {
 
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+      
+    </BrowserRouter>
   )
 }
 
