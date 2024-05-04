0acb31e786edd66fb3a8d40c597fbe1dd6b729cc
sasanka003 <sasankaudana@gmail.com>
2024-05-04 22:51:38 +0530

    header links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Header from './components/Header';
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
