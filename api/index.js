49b1a6fe1f89216ade2c3f14307a76374f06d2f8
sasanka003 <sasankaudana@gmail.com>
2024-05-05 23:51:02 +0530

    added database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 const app = express();
 const port = 3000;
