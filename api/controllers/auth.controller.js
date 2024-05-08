25dea80ba27e2cebef45f7821d6ed08b5bbf1bab
sasanka003 <sasankaudana@gmail.com>
2024-05-09 00:05:39 +0530

    error middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   // console.log(req.body);
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   } 
 
   const hashPassword = bcryptjs.hashSync(password, 10);
@@ -19,10 +20,9 @@ export const signup = async (req, res) => {
 
     try {
         await newUser.save();
-        return res.status(201).json({ message: "User created successfully" });
+        res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        return res.status(500).json({ message: error.message });
-    }
+        next(error);}
 };
 
 export const login = async (req, res) => {
