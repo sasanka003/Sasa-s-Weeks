0418b8dd3a8fb810f4183cb91f789bbfc10e1b0e
sasanka003 <sasankaudana@gmail.com>
2024-05-10 13:21:19 +0530

    Signin frontend

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,112 @@
-import React from 'react'
+import { useState } from 'react';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Signin() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value.trim(),
+    });
+    // console.log(formData);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if(!formData.email || !formData.password) {
+      return setErrorMessage('Please fill in all fields!');
+    }
+    if(formData.password.length < 6) {
+      return setErrorMessage('Password must be at least 6 characters long!');
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const response = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if(data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.Message);
+      }
+      setLoading(false);
+      if(response.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      // console.error(error);
+      setErrorMessage(error.Message);
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>Signin</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        <div className='flex-1'>
+        <Link to='/' className='text-4xl font-bold dark:text-white'>
+          <span className='px-2 py-1 bg-gradient-to-r from-green-400 to-blue-500 rounded-lg text-white'>
+            Sasa's
+          </span>
+          Blog
+        </Link>
+        <p className='text-sm mt-5'>
+          This is a demo project. You can sign in with your email and password
+          or with Google.
+        </p>
+        </div>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange}/>
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
+            </div>
+            <Button gradientDuoTone='purpleToBlue' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
+                  </>
+                ) : (
+                  'Sign In'
+                )
+              }
+            </Button>
+          </form>
+          <div className='flex gap-2 textsm mt-5'>
+            <span>Don't have an account?</span>
+            <Link to='/signup' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    </div>
   )
 }
+
