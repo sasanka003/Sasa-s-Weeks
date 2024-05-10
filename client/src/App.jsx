fd472d79d2578c3946854bcc4da27870c70ac0bc
sasanka003 <sasankaudana@gmail.com>
2024-05-10 11:24:08 +0530

    Footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './pages/Projects';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -20,7 +21,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
-      
+      <Footer/>
     </BrowserRouter>
   )
 }
