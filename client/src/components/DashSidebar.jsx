dbe09d4a1f724bca41d886c70ab1ddb7de5a0e4f
sasanka003 <sasankaudana@gmail.com>
2024-05-20 23:32:43 +0530

    Complete image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -4,23 +4,31 @@ import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function DashSidebar() {
-    const location = useLocation();
-    const [tab, setTab] = useState('');
-    useEffect(() => {
-      const urlParams = new URLSearchParams(location.search);
-      const tabFromUrl = urlParams.get('tab');
-      if (tabFromUrl) {
-        setTab(tabFromUrl);
-      }
-    }, [location.search]);
-    return (
-        <Sidebar className="w-full md:w-56">
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} labelColor='dark' >Profile</Sidebar.Item>
-                <Sidebar.Item icon={HiArrowSmRight}>Sign Out</Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
-        </Sidebar>
-    );
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup>
+          <Sidebar.Item
+            active={tab === "profile"}
+            icon={HiUser}
+            label={"User"}
+            labelColor="dark"
+            as='div'
+          >
+            Profile
+          </Sidebar.Item>
+          <Sidebar.Item icon={HiArrowSmRight}>Sign Out</Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
 }
