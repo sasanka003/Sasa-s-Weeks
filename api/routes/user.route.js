f1ae4421725ecf6df923bee876fc06f2f50f1650
sasanka003 <sasankaudana@gmail.com>
2024-05-21 12:57:14 +0530

    delete user API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get('/test', test);
 router.put('/update/:userId', verifyUser, updateUser);
+router.delete('/delete/:userId', verifyUser, deleteUser);
 
 export default router;
