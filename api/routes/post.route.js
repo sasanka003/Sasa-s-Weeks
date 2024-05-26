0be1bd2c11163dc00321217ad010b8f5eee86a87
Sasanka003 <sasankaudana@gmail.com>
2024-05-26 22:46:14 +0530

    get posts api

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyUser } from '../utils/verifyUser.js';
-import { createPost, updatePost, deletePost } from '../controllers/post.controller.js';
+import { createPost, getPosts, updatePost, deletePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createPost);
+router.get('/getPosts', getPosts);
 router.put('/update/:postId', verifyUser, updatePost);
 router.delete('/delete/:postId', verifyUser, deletePost);
 
