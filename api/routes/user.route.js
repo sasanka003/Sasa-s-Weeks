d6979f5d4cca3155793c7638d31360d00e1a9c75
Sasanka003 <sasankaudana@gmail.com>
2024-05-27 14:21:34 +0530

    get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signout } from '../controllers/user.controller.js';
+import { test, getUsers, updateUser, deleteUser, signout } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
+router.get('/getusers', verifyUser, getUsers);
 router.put('/update/:userId', verifyUser, updateUser);
 router.delete('/delete/:userId', verifyUser, deleteUser);
 router.post('/signout', signout);
