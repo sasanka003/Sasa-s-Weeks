da0d1ebe47cef29804f7d4c827d26c73b37e959b
Sasanka003 <sasankaudana@gmail.com>
2024-05-30 23:57:58 +0530

    comment header

---
+++ client/src/components/CommentSection.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+export default function CommentSection({ PostId }) {
+  const { currentUser } = useSelector(state => state.user); 
+  return (
+    <div className='max-w-2xl mx-auto w-full p-3'>
+      {currentUser ? (
+        <div className='flex items-center gap-1 my-5 text-gray-500 text-sm'>
+          <p>Signed in as:</p>
+          <img className='h-5 w-5 object-cover rounded-full' src={currentUser.profilePicture} alt={currentUser.username} />
+          <Link className='text-xs text-cyan-600 hover:underline' to={`/dashboard?tab=profile`}>@{currentUser.username}</Link>
+        </div>
+        
+      ) : (
+        <div className=''>
+          You must sign in to comment.
+          <Link to={'/sign-in'}>Login</Link>
+        </div>
+      )}
+    </div>
+  )
+}
