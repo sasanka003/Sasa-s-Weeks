20492553c9def0497f01aac94d585731b3a72e16
Sasanka003 <sasankaudana@gmail.com>
2024-06-05 23:49:24 +0530

    added search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute/>}>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route element={<OnlyEditorPrivateRoute />}>
