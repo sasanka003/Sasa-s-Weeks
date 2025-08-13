02c01caf6c8a805431fac03be5c227a82674799c
Sasanka003 <sasankaudana@gmail.com>
2025-08-13 15:22:37 +0530

    ci/cd for frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-    },
+    open: true,
+    port: 3001
   },
   plugins: [react()],
+  base: "Sasa_s_weeks",
 })
